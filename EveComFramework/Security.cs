9f703cf2a8023eae17fbcea4203acdf668570289
Tehtsuo <Tehtsuo@gmail.com>
2013-04-09 15:13:52 -0400

    Getting started

---
+++ EveComFramework/Security.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EveCom;
+
+namespace EveComFramework
+{
+    // Define a class to hold custom event info 
+    internal class AlertArg : EventArgs
+    {
+        public enum EventType
+        {
+            Pod,
+            NegativeStanding,
+            NeutralStanding,
+            Targeted,
+            CapacitorLow,
+            ShieldLow,
+            ArmorLow
+        }
+
+        public EventType trigger { get; set; }
+        public AlertArg(EventType trigger)
+        {
+            this.trigger = trigger;
+        }
+
+    }
+
+
+    class Security : State
+    {
+
+        #region Instantiation
+
+        #endregion
+
+        #region Variables
+
+        List<Bookmark> SafeSpots;
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler<AlertArg> Alert;
+
+
+        protected virtual void TriggerAlert(AlertArg e)
+        {
+            EventHandler<AlertArg> handler = Alert;
+
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+
+        #endregion
+
+        #region Actions
+
+        public override void Start()
+        {
+            if (Idle)
+            {
+                QueueState(CheckSafe);
+            }
+
+        }
+
+        public override void Stop()
+        {
+            Clear();
+        }
+
+        #endregion
+
+        #region States
+
+        bool CheckSafe(object[] Params)
+        {
+            if (!Standing.Ready)
+            {
+                Standing.LoadStandings();
+                return false;
+            }
+
+            foreach (string FleeTrigger in Config.FleeTriggers)
+            {
+                switch (FleeTrigger)
+                {
+                    case "In Pod":
+                        if (MyShip.ToItem.GroupID == Group.Capsule)
+                        {
+                            TriggerAlert(new AlertArg(AlertArg.EventType.Pod));
+                            return true;
+                        }
+                        break;
+                    case "Negative Standing in Local":
+                        List<Pilot> NegativePilots = Local.Pilots.Where(a => (a.ToAlliance.FromAlliance < 0 ||
+                                                                                a.ToAlliance.FromCorp < 0 ||
+                                                                                a.ToAlliance.FromChar < 0 ||
+                                                                                a.ToCorp.FromAlliance < 0 ||
+                                                                                a.ToCorp.FromCorp < 0 ||
+                                                                                a.ToCorp.FromChar < 0 ||
+                                                                                a.ToChar.FromAlliance < 0 ||
+                                                                                a.ToChar.FromCorp < 0 ||
+                                                                                a.ToChar.FromChar < 0
+                                                                             ) &&
+                                                                             a.ID != Me.CharID).ToList();
+                        if (!Config.NegativeAlliance) { NegativePilots.RemoveAll(a => a.AllianceID == Me.AllianceID); }
+                        if (!Config.NegativeCorp) { NegativePilots.RemoveAll(a => a.CorpID == Me.CorpID); }
+                        if (!Config.NegativeFleet) { NegativePilots.RemoveAll(a => a.IsFleetMember); }
+                        if (NegativePilots.Count > 0)
+                        {
+                            TriggerAlert(new AlertArg(AlertArg.EventType.NegativeStanding));
+                            return true;
+                        }
+                        break;
+                    case "Neutral in Local":
+                        List<Pilot> NeutralPilots = Local.Pilots.Where(a => (a.ToAlliance.FromAlliance <= 0 &&
+                                                                                a.ToAlliance.FromCorp <= 0 &&
+                                                                                a.ToAlliance.FromChar <= 0 &&
+                                                                                a.ToCorp.FromAlliance <= 0 &&
+                                                                                a.ToCorp.FromCorp <= 0 &&
+                                                                                a.ToCorp.FromChar <= 0 &&
+                                                                                a.ToChar.FromAlliance <= 0 &&
+                                                                                a.ToChar.FromCorp <= 0 &&
+                                                                                a.ToChar.FromChar <= 0
+                                                                             ) &&
+                                                                             a.ID != Me.CharID).ToList();
+                        if (!Config.NeutralAlliance) { NeutralPilots.RemoveAll(a => a.AllianceID == Me.AllianceID); }
+                        if (!Config.NeutralCorp) { NeutralPilots.RemoveAll(a => a.CorpID == Me.CorpID); }
+                        if (!Config.NeutralFleet) { NeutralPilots.RemoveAll(a => a.IsFleetMember); }
+                        if (NeutralPilots.Count > 0)
+                        {
+                            TriggerAlert(new AlertArg(AlertArg.EventType.NeutralStanding));
+                            return true;
+                        }
+                        break;
+                    case "Targeted by another player":
+                        if (!Session.InSpace)
+                        {
+                            break;
+                        }
+                        List<Pilot> TargetingPilots = Local.Pilots.Where(a => Entity.All.FirstOrDefault(b => b.CharID == a.ID && b.IsTargetingMe) != null).ToList();
+                        if (!Config.TargetAlliance) { TargetingPilots.RemoveAll(a => a.AllianceID == Me.AllianceID); }
+                        if (!Config.TargetCorp) { TargetingPilots.RemoveAll(a => a.CorpID == Me.CorpID); }
+                        if (!Config.TargetFleet) { TargetingPilots.RemoveAll(a => a.IsFleetMember); }
+                        if (TargetingPilots.Count > 0)
+                        {
+                            TriggerAlert(new AlertArg(AlertArg.EventType.Targeted));
+                            return true;
+                        }
+                        break;
+                    case "Capacitor Low":
+                        if (!Session.InSpace)
+                        {
+                            break;
+                        }
+                        if ((MyShip.Capacitor / MyShip.MaxCapacitor * 100) < Config.CapThreshold)
+                        {
+                            TriggerAlert(new AlertArg(AlertArg.EventType.CapacitorLow));
+                            return true;
+                        }
+                        break;
+                    case "Shield Low":
+                        if (!Session.InSpace)
+                        {
+                            break;
+                        }
+                        if (MyShip.ToEntity.ShieldPct < Config.ShieldThreshold)
+                        {
+                            TriggerAlert(new AlertArg(AlertArg.EventType.ShieldLow));
+                            return true;
+                        }
+                        break;
+                    case "Armor Low":
+                        if (!Session.InSpace)
+                        {
+                            break;
+                        }
+                        if (MyShip.ToEntity.ArmorPct < Config.ArmorThreshold)
+                        {
+                            TriggerAlert(new AlertArg(AlertArg.EventType.ArmorLow));
+                            return true;
+                        }
+                        break;
+                }
+            }
+
+            return false;
+        }
+
+        public void Flee()
+        {
+            if (Session.InStation)
+            {
+                return;
+            }
+
+            foreach (string FleeType in Config.FleeTypes)
+            {
+                switch (FleeType)
+                {
+                    case "Flee to closest station in system":
+                        if (Entity.All.FirstOrDefault(a => a.GroupID == Group.Station) != null)
+                        {
+                            Move.Object(Entity.All.FirstOrDefault(a => a.GroupID == Group.Station));
+                            return true;
+                        }
+                        break;
+                    case "Flee to secure bookmark":
+                        if (Bookmark.All.Count(a => a.Title == Config.SecureBookmark) > 0)
+                        {
+                            Move.Bookmark(Bookmark.All.FirstOrDefault(a => a.Title == Config.SecureBookmark));
+                            return true;
+                        }
+                        break;
+                    case "Cycle safe bookmarks":
+                        if (SafeSpots.Count == 0)
+                        {
+                            SafeSpots = Bookmark.All.Where(a => a.Title.Contains(Config.SafeSubstring) && a.LocationID == Session.SolarSystemID).ToList();
+                        }
+                        if (SafeSpots.Count > 0)
+                        {
+                            Move.Bookmark(SafeSpots.FirstOrDefault());
+                            SafeSpots.Remove(SafeSpots.FirstOrDefault());
+                            return true;
+                        }
+                        break;
+                    case "Flee to closest station outside system":
+                        break;
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ EveComFramework/State.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EveCom;
+
+namespace EveComFramework
+{
+    internal class State
+    {
+        internal class StateQueue
+        {
+            internal Func<object[], bool> State { get; set; }
+            internal object[] Params { get; set; }
+            internal int Frequency { get; set; }
+            internal StateQueue(Func<object[], bool> State, int Frequency, object[] Params)
+            {
+                this.State = State;
+                this.Params = Params;
+                this.Frequency = Frequency;
+            }
+        }
+
+        public int DefaultFrequency { get; set; }
+        public DateTime NextPulse { get; set; }
+        internal LinkedList<StateQueue> States = new LinkedList<StateQueue>();
+        internal StateQueue CurState;
+        public bool Idle { get { return CurState == null; } }
+
+        public State()
+        {
+            DefaultFrequency = 1000;
+            EVEFrame.OnFrame += OnFrame;
+        }
+
+        ~State()
+        {
+            EVEFrame.OnFrame -= OnFrame;
+        }
+
+        public void QueueState(Func<object[], bool> State, int Frequency = -1, params object[] Params)
+        {
+            States.AddFirst(new StateQueue(State, ((Frequency == -1) ? DefaultFrequency : Frequency), Params));
+            if (CurState == null)
+            {
+                CurState = States.Last();
+                States.RemoveLast();
+            }
+        }
+
+        public void InsertState(Func<object[], bool> State, int Frequency = -1, params object[] Params)
+        {
+            States.AddLast(new StateQueue(State, ((Frequency == -1) ? DefaultFrequency : Frequency), Params));
+            if (CurState == null)
+            {
+                CurState = States.Last();
+                States.RemoveLast();
+            }
+        }
+
+        public void DislodgeCurState(Func<object[], bool> State, int Frequency = -1, params object[] Params)
+        {
+            if (CurState != null)
+            {
+                States.AddLast(CurState);
+            }
+            CurState = new StateQueue(State, ((Frequency == -1) ? DefaultFrequency : Frequency), Params);
+        }
+
+        public void Clear()
+        {
+            States.Clear();
+            CurState = null;
+        }
+
+        public void ClearCurState()
+        {
+            CurState = null;
+            if (States.Count > 0)
+            {
+                CurState = States.Last();
+                States.RemoveLast();
+            }
+        }
+
+        public bool WaitForState(object[] Params)
+        {
+            switch (Params.Length)
+            {
+                case 1:
+                    if ((int)Params[0] >= 1)
+                    {
+                        CurState.Params[0] = (int)Params[0] - 1;
+                        return false;
+                    }
+                    break;
+                case 2:
+                    if ((int)Params[0] >= 1 && !((Func<bool>)Params[1])())
+                    {
+                        CurState.Params[0] = (int)Params[0] - 1;
+                        return false;
+                    }
+                    break;
+                case 4:
+                    if ((int)Params[0] >= 1 && !((Func<bool>)Params[1])())
+                    {
+                        if (((Func<bool>)Params[2])())
+                        {
+                            CurState.Params[0] = (int)Params[3];
+                        }
+                        else
+                        {
+                            CurState.Params[0] = (int)Params[0] - 1;
+                        }
+                        return false;
+                    }
+                    break;
+            }
+            return true;
+        }
+
+        public void WaitFor(int TimeOut, Func<bool> Test = null, Func<bool> Reset = null)
+        {
+            if (Reset != null)
+            {
+                InsertState(WaitForState, -1, TimeOut, Test, Reset, TimeOut);
+            }
+            else if (Test != null)
+            {
+                InsertState(WaitForState, -1, TimeOut, Test);
+            }
+            else
+            {
+                InsertState(WaitForState, -1, TimeOut);
+            }
+        }
+
+        void OnFrame(object sender, EventArgs e)
+        {
+            Random Delta = new Random();
+            if (DateTime.Now > NextPulse.AddMilliseconds(Delta.Next(100)))
+            {
+                if (CurState != null && Session.Safe && Session.NextSessionChange < Session.Now)
+                {
+                    if (CurState.State(CurState.Params))
+                    {
+                        if (States.Count > 0)
+                        {
+                            CurState = States.Last();
+                            States.RemoveLast();
+                        }
+                        else
+                        {
+                            CurState = null;
+                        }
+                    }
+                }
+
+                if (CurState == null)
+                {
+                    NextPulse = DateTime.Now.AddMilliseconds(DefaultFrequency);
+                }
+                else
+                {
+                    NextPulse = DateTime.Now.AddMilliseconds(CurState.Frequency);
+                }
+            }
+        }
+
+
+    }
+
+}
