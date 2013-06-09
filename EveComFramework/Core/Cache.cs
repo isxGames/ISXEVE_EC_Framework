662dc3ed1a044cc1c93f23825d34769025170ac1
Tehtsuo <TimLeap@gmail.com>
2013-06-09 16:16:48 -0400

    Add ItemCache dictionary with types and volumes

--- EveComFramework/Core/Cache.cs
+++ EveComFramework/Core/Cache.cs
@@ -36,6 +36,7 @@ namespace EveComFramework.Core
         public string[] Bookmarks { get; set; }
         public string[] FleetMembers { get; set; }
         public string[] CargoItems { get; set; }
+        public Dictionary<string, double> ItemCache { get; set; }
 
         #endregion
 
@@ -44,13 +45,48 @@ namespace EveComFramework.Core
         bool Control(object[] Params)
         {
             if ((!Session.InSpace && !Session.InStation) || !Session.Safe) return false;
+            if (ItemCache == null) ItemCache = new Dictionary<string, double>();
             Name = Me.Name;
             Bookmarks = Bookmark.All.Select(a => a.Title).ToArray();
             if (Session.InFleet) FleetMembers = Fleet.Members.Select(a => a.Name).ToArray();
             if (MyShip.CargoBay != null && MyShip.CargoBay.IsPrimed) CargoItems = MyShip.CargoBay.Items.Distinct().Select(a => a.Type).ToArray();
+            if (MyShip.CargoBay != null && MyShip.CargoBay.IsPrimed) MyShip.CargoBay.Items.ForEach(a => { ItemCache.AddOrUpdate(a.Type, a.Volume); });
+            if (Session.InStation && Station.ItemHangar != null && Station.ItemHangar.IsPrimed) Station.ItemHangar.Items.ForEach(a => { ItemCache.AddOrUpdate(a.Type, a.Volume); });
             return false;
         }
 
         #endregion
     }
+
+    #region Utility classes
+
+    static class DictionaryHelper
+    {
+        public static IDictionary<TKey, TValue> AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key] = value;
+            }
+            else
+            {
+                dictionary.Add(key, value);
+            }
+
+            return dictionary;
+        }
+    }
+
+    public static class ForEachExtension
+    {
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> method)
+        {
+            foreach (T item in items)
+            {
+                method(item);
+            }
+        }
+    }
+
+    #endregion
 }
