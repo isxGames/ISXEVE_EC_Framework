8e4a45d58e02bb984b0c3b87dbc7f5807ba98991
Tehtsuo <Tehtsuo@gmail.com>
2013-06-11 09:09:28 -0400

    Document Busy.

--- EveComFramework/Core/Busy.cs
+++ EveComFramework/Core/Busy.cs
@@ -5,11 +5,17 @@ using System.Text;
 
 namespace EveComFramework.Core
 {
+    /// <summary>
+    /// This class allows modules to signal they are busy
+    /// </summary>
     public class Busy
     {
         #region Instantiation
 
         static Busy _Instance;
+        /// <summary>
+        /// Singletoner
+        /// </summary>
         public static Busy Instance
         {
             get
@@ -30,6 +36,9 @@ namespace EveComFramework.Core
         
         static Dictionary<string, Action> BusySet = new Dictionary<string, Action>();
 
+        /// <summary>
+        /// Property indicating if any module is busy
+        /// </summary>
         public bool IsBusy
         {
             get
@@ -38,11 +47,21 @@ namespace EveComFramework.Core
             }
         }
 
+        /// <summary>
+        /// Property indicating if a specific module is busy
+        /// </summary>
+        /// <param name="Item">The name of the module</param>
+        /// <returns>Busy true/false</returns>
         public bool ItemIsBusy(string Item)
         {
             return BusySet.ContainsKey(Item);
         }
 
+        /// <summary>
+        /// Set a module as busy
+        /// </summary>
+        /// <param name="Item">The name of the module</param>
+        /// <param name="Signal">Action to perform when a module signals ready</param>
         public void SetBusy(string Item, Action Signal)
         {
             if (!BusySet.ContainsKey(Item))
@@ -51,6 +70,10 @@ namespace EveComFramework.Core
             }
         }
 
+        /// <summary>
+        /// Set a module as not busy
+        /// </summary>
+        /// <param name="Item">The name of the module</param>
         public void SetDone(string Item)
         {
             if (BusySet.ContainsKey(Item))
@@ -59,6 +82,9 @@ namespace EveComFramework.Core
             }
         }
         
+        /// <summary>
+        /// Signal modules that are ready using the actions defined
+        /// </summary>
         public void SignalReady()
         {
             BusySet.Values.ToList().ForEach(signal => signal());
