2d9369a73c9f7256a8488cc207300404da2919e7
Tehtsuo <Tehtsuo@gmail.com>
2013-07-11 07:49:48 -0400

    Starting rough design

--- EveComFramework/SimpleDrone/SimpleDrone.cs
+++ EveComFramework/SimpleDrone/SimpleDrone.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using EveCom;
 using EveComFramework.Core;
+using EveComFramework.Targets;
 
 namespace EveComFramework.SimpleDrone
 {
     #region Settings
 
-    public class DroneControlSettings : EveComFramework.Core.Settings
+    public class LocalSettings : EveComFramework.Core.Settings
     {
         public bool ShortRangeClear = true;
         public bool LongRangeClear = false;
@@ -18,7 +19,7 @@ namespace EveComFramework.SimpleDrone
 
     #endregion
 
-    class SimpleDrone : State
+    public class SimpleDrone : State
     {
         #region Instantiation
 
@@ -37,11 +38,72 @@ namespace EveComFramework.SimpleDrone
 
         private SimpleDrone() : base()
         {
+            Rats.AddPriorityTargets();
+            Rats.AddNPCs();
+            Rats.AddTargetingMe();
 
+            Rats.Ordering = new RatComparer();            
         }
 
         #endregion
 
+        #region Variables
 
+        public Core.Logger Log = new Core.Logger("SimpleDrone");
+        public LocalSettings Config = new LocalSettings();
+        Targets.Targets Rats = new Targets.Targets();
+        public Dictionary<long, long> ActiveTargetList = new Dictionary<long, long>();
+
+        #endregion
+
+        #region Actions
+
+        public void Enabled(bool var)
+        {
+            if (var)
+            {
+                if (Idle)
+                {
+                    QueueState(Control);
+                }
+            }
+            else
+            {
+                Clear();
+            }
+        }
+
+        public void Configure()
+        {
+            //UI.DroneControl Configuration = new UI.DroneControl();
+            //Configuration.Show();
+        }
+
+        #endregion
+
+        #region States
+
+        Entity ActiveTarget;
+
+        bool Control(object[] Params)
+        {
+            if (!Session.InSpace)
+            {
+                return false;
+            }
+
+            // If we're warping and drones are in space, recall them and stop the module
+            if (MyShip.ToEntity.Mode == EntityMode.Warping && Drone.AllInSpace.Any())
+            {
+                Drone.AllInSpace.ReturnToDroneBay();
+                return true;
+            }
+
+
+
+            return false;
+        }
+
+        #endregion
     }
 }
