4b447eed06e47ea25caac075ef87666ff4dcfec0
Tehtsuo <Tehtsuo@gmail.com>
2013-07-11 07:30:46 -0400

    Starting SimpleDrone class.  Replacement for DroneControl, will be more specialized, easier to configure, and will not only manage deployment and control of drones, not retrieval.  This will rely on bots to retrieve drones, which should improve reliability.

---
+++ EveComFramework/SimpleDrone/SimpleDrone.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EveCom;
+using EveComFramework.Core;
+
+namespace EveComFramework.SimpleDrone
+{
+    #region Settings
+
+    public class DroneControlSettings : EveComFramework.Core.Settings
+    {
+        public bool ShortRangeClear = true;
+        public bool LongRangeClear = false;
+        public bool Sentry = false;
+    }
+
+    #endregion
+
+    class SimpleDrone : State
+    {
+        #region Instantiation
+
+        static SimpleDrone _Instance;
+        public static SimpleDrone Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new SimpleDrone();
+                }
+                return _Instance;
+            }
+        }
+
+        private SimpleDrone() : base()
+        {
+
+        }
+
+        #endregion
+
+
+    }
+}
