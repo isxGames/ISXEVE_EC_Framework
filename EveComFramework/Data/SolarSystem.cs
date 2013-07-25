bc7620d93f46c97f00758a1b4554cfdd1ddb0c32
Tehtsuo <Tehtsuo@gmail.com>
2013-07-25 10:06:46 -0400

    Use Culture.InvariantCulture for Convert.ToDouble

--- EveComFramework/Data/SolarSystem.cs
+++ EveComFramework/Data/SolarSystem.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Xml;
 using System.Xml.Linq;
 using System.Reflection;
+using System.Globalization;
 
 namespace EveComFramework.Data
 {
@@ -55,7 +56,7 @@ namespace EveComFramework.Data
                     {
                         XElement dataDoc = XElement.Load(data);
                         _All = (from System in dataDoc.Descendants("System")
-                                select new SolarSystem(Convert.ToInt64(System.Attribute("ID").Value), System.Attribute("Name").Value, Convert.ToDouble(System.Attribute("Security").Value), Convert.ToInt64(System.Attribute("FactionID").Value))).ToList();
+                                select new SolarSystem(Convert.ToInt64(System.Attribute("ID").Value), System.Attribute("Name").Value, Convert.ToDouble(System.Attribute("Security").Value, CultureInfo.InvariantCulture), Convert.ToInt64(System.Attribute("FactionID").Value))).ToList();
                     }
                 }
                 return _All;
