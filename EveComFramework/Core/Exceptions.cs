c80436d1cb238347df8128a7e9eab384e7f33f28
Tehtsuo <TimLeap@gmail.com>
2014-02-26 18:44:40 -0500

    Add Exceptions.cs

---
+++ EveComFramework/Core/Exceptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+namespace EveComFramework.Core
+{
+    public class Exceptions
+    {
+        string file { get; set; }
+
+        public void Post(string title, Exception val)
+        {
+            string LogDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\logs\\";
+
+
+            if (!Directory.Exists(LogDirectory))
+            {
+                Directory.CreateDirectory(LogDirectory);
+            }
+
+            if (file == null) file = LogDirectory + title + " - " + DateTime.Now.Ticks + ".txt";
+            StreamWriter oWriter = new StreamWriter(file, true);
+            oWriter.Write(val.Message + Environment.NewLine + Environment.NewLine + val.StackTrace + Environment.NewLine + Environment.NewLine);
+            oWriter.Close();
+        }
+    }
+}
