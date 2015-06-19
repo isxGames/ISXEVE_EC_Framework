4763c167097eedb9852ecf440b0c430a685e9492
金田 <kaneda@dev.null>
2015-06-20 05:51:51 +0900

    Add PreferredBookmarkExtension to KanedaToolkit

---
+++ EveComFramework/KanedaToolkit/PreferredBookmarkExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EveCom;
+
+namespace EveComFramework.KanedaToolkit
+{
+
+    public static class PreferredBookmarkExtension
+    {
+        public static Bookmark PreferredBookmark(this IEnumerable<Bookmark> items, Func<Bookmark, bool> pred)
+        {
+            Bookmark inSystem = items.Where(pred).FirstOrDefault(a => a.LocationID == Session.SolarSystemID);
+            if (inSystem != null)
+            {
+                return inSystem;
+            }
+            else
+            {
+                return items.FirstOrDefault();
+            }
+        }
+    }
+
+}
