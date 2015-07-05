a9c64dd9b75dc40cee5d76f6f9cdc79812df080f
Vinaya <vinaya@example.com>
2015-07-05 12:29:13 +0200

    Add BotUI baseclass Fix Inactive state

--- EveComFramework/KanedaToolkit/Bot.cs
+++ EveComFramework/KanedaToolkit/Bot.cs
@@ -1,4 +1,6 @@
 ﻿#pragma warning disable 1591
+using System;
+using System.Windows.Forms;
 using EveCom;
 using EveComFramework.Core;
 
@@ -22,12 +24,12 @@ namespace EveComFramework.KanedaToolkit
         {
             get
             {
-                if(Idle) return BotStatus.StatusInactive;
                 if (SecurityModule != null)
                 {
                     if (SecurityModule.IsPanic) return BotStatus.StatusActivePanic;
                     if (SecurityModule.IsAlert) return BotStatus.StatusActiveAlert;
                 }
+                if (Idle) return BotStatus.StatusInactive;
                 return BotStatus.StatusActive;
             }
         }
@@ -71,5 +73,40 @@ namespace EveComFramework.KanedaToolkit
             return false;
         }
         #endregion
+    }
+
+    public class BotUI : Form
+    {
+        protected Bot Bot = null;
+        private Timer wuTimer = new Timer();
+
+        public BotUI()
+        {
+            wuTimer.Enabled = true;
+            wuTimer.Interval = 100;
+            wuTimer.Start();
+            wuTimer.Tick += wuTimer_Tick;
+        }
+
+        private void wuTimer_Tick(object o, EventArgs e)
+        {
+            if (Bot != null)
+            {
+                switch (Bot.Status)
+                {
+                    case Bot.BotStatus.StatusInactive:
+                        TaskbarProgress.SetState(Handle, TaskbarProgress.TaskbarStates.NoProgress);
+                        break;
+                    case Bot.BotStatus.StatusActive:
+                        TaskbarProgress.SetState(Handle, TaskbarProgress.TaskbarStates.Normal, 100, 100);
+                        break;
+                    case Bot.BotStatus.StatusActiveAlert:
+                    case Bot.BotStatus.StatusActivePanic:
+                        TaskbarProgress.SetState(Handle, TaskbarProgress.TaskbarStates.Error, 100, 100);
+                        break;
+                }
+            }
+        }
+
     }
 }
