4020c36b0be24e0ff23c04d2efd3936fcb20ac44
Vinaya <vinaya@example.com>
2015-09-30 10:40:53 +0200

    Add faction standings for agent access

--- EveComFramework/KanedaToolkit/KAgent.cs
+++ EveComFramework/KanedaToolkit/KAgent.cs
@@ -19,10 +19,10 @@ namespace EveComFramework.KanedaToolkit
                 return false;
             }
             if (agent.Level > 1 && Standing.NPCStanding(agent.FactionID, true) < -2.00) return false;
-            if (agent.Level == 2 && Standing.NPCStanding(agent.CorporationID, true) > 1.00) return true;
-            if (agent.Level == 3 && Standing.NPCStanding(agent.CorporationID, true) > 3.00) return true;
-            if (agent.Level == 4 && Standing.NPCStanding(agent.CorporationID, true) > 5.00) return true;
-            if (agent.Level == 5 && Standing.NPCStanding(agent.CorporationID, true) > 7.00) return true;
+            if (agent.Level == 2 && (Standing.NPCStanding(agent.CorporationID, true) > 1.00 || Standing.NPCStanding(agent.FactionID, true) > 1.00)) return true;
+            if (agent.Level == 3 && (Standing.NPCStanding(agent.CorporationID, true) > 3.00 || Standing.NPCStanding(agent.FactionID, true) > 3.00)) return true;
+            if (agent.Level == 4 && (Standing.NPCStanding(agent.CorporationID, true) > 5.00 || Standing.NPCStanding(agent.FactionID, true) > 5.00)) return true;
+            if (agent.Level == 5 && (Standing.NPCStanding(agent.CorporationID, true) > 7.00 || Standing.NPCStanding(agent.FactionID, true) > 7.00)) return true;
             return false;
         }
 
