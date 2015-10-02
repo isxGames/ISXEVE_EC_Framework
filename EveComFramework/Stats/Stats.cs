cde244a00457015607c443940e59b76fb9d62bf8
Vinaya <vinaya@example.com>
2015-10-02 14:52:07 +0200

    Stats: Add customs office ownership mapping (not enabled by default)

--- EveComFramework/Stats/Stats.cs
+++ EveComFramework/Stats/Stats.cs
@@ -1,5 +1,8 @@
 #pragma warning disable 1591
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Net;
 using EveCom;
 using EveComFramework.Core;
@@ -41,6 +44,7 @@ namespace EveComFramework.Stats
         #region Variables
         public StatsSettings Config = new StatsSettings();
         Logger Log = new Logger("Stats");
+        private String StatsHost = "http://104.238.149.13/evecom-stats/";
         #endregion
 
         #region States
@@ -64,17 +68,44 @@ namespace EveComFramework.Stats
             if (Config.optIn) // detailed data allowed
             {
                 data = data + String.Format(@"&solarSystemID={0}&characterID={1}&typeID={2}", Session.SolarSystemID, Me.CharID, MyShip.ToItem.TypeID);
+                QueueState(DatabaseFeeder);
             }
 
             try
             {
-                WebRequest.Create("http://104.238.149.13/evecom-stats/?" + data).GetResponse();
+                WebRequest.Create(StatsHost+"?" + data).GetResponse();
             }
             catch
             {
                 Log.Log("|rNetwork connection failed");
             }
             return true;
+        }
+
+        private List<long> CustomsOffices = new List<long>();
+        private bool DatabaseFeeder(object[] Params)
+        {
+            if (Session.Safe && Session.InSpace)
+            {
+                List<Entity> ReportCustomsOffices = Entity.All.Where(a => (a.TypeID == 2233 || a.TypeID == 4318) && !CustomsOffices.Contains(a.ID)).ToList();
+                if (ReportCustomsOffices.Any())
+                {
+                    Entity POCO = ReportCustomsOffices.First();
+                    String data = String.Format(@"GUID={0}&solarSystemID={1}&ownerID={2}&itemID={3}&typeID={4}&x={5}&y={6}&z={7}", Config.guid, Session.SolarSystemID, POCO.OwnerID, POCO.ID, POCO.TypeID, POCO.Position.X.ToString(CultureInfo.InvariantCulture), POCO.Position.Y.ToString(CultureInfo.InvariantCulture), POCO.Position.Z.ToString(CultureInfo.InvariantCulture));
+                    try
+                    {
+                        Log.Log("Submit POCO data: " + data, LogType.DEBUG);
+                        WebRequest.Create(StatsHost + "poco.php?" + data).GetResponse();
+                        CustomsOffices.Add(POCO.ID);
+                    }
+                    catch
+                    {
+                        Log.Log("|rNetwork connection failed");
+                        return true; // Stop reporting stuff if there is no reliable network connection
+                    }
+                }
+            }
+            return false;
         }
         #endregion
     }
