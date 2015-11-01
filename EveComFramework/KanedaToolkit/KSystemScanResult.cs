7c1caa982d9c63da1ff6e62214699677daebda69
Vinaya <vinaya@example.com>
2015-11-01 19:38:22 +0100

    Add KSystemScannerResult.SiteTaken()

---
+++ EveComFramework/KanedaToolkit/KSystemScanResult.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable 1591
+using System.Linq;
+using EveCom;
+
+namespace EveComFramework.KanedaToolkit
+{
+    /// <summary>
+    /// extension methods for SystemScanResult
+    /// </summary>
+    public static class KSystemScanResult
+    {
+        /// <summary>
+        /// Is this site taken?
+        /// </summary>
+        public static bool SiteTaken(this SystemScanResult SystemScanResult)
+        {
+            return Comms.Comms.Instance.LocalChat.Messages.Any(a => a.Time.AddHours(1) > Session.Now && a.Text.ToUpper() == SystemScanResult.ID.Substring(0, 3) && a.SenderName != Me.Name && (!Session.InFleet || !Fleet.Members.Any(b => b.Name == a.SenderName)));
+        }
+
+    }
+
+}
