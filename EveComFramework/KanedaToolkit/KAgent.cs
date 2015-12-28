335653339b4d7c586b061ef5f8677450c76efb84
Vinaya <vinaya@example.com>
2015-12-28 02:38:26 +0100

    Fix Level 1 mission standing requirements

--- EveComFramework/KanedaToolkit/KAgent.cs
+++ EveComFramework/KanedaToolkit/KAgent.cs
@@ -19,6 +19,7 @@ namespace EveComFramework.KanedaToolkit
                 return false;
             }
             if (agent.Level > 1 && Standing.NPCStanding(agent.FactionID, true) < -2.00) return false;
+            if (agent.Level == 1) return true;
             if (agent.Level == 2 && (Standing.NPCStanding(agent.CorporationID, true) > 1.00 || Standing.NPCStanding(agent.FactionID, true) > 1.00)) return true;
             if (agent.Level == 3 && (Standing.NPCStanding(agent.CorporationID, true) > 3.00 || Standing.NPCStanding(agent.FactionID, true) > 3.00)) return true;
             if (agent.Level == 4 && (Standing.NPCStanding(agent.CorporationID, true) > 5.00 || Standing.NPCStanding(agent.FactionID, true) > 5.00)) return true;
