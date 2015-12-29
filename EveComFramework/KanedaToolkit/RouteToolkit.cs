0bf8cca0cf88760f867632e57f35bd37c04a2f85
金田 <kaneda@dev.null>
2015-12-29 08:47:23 +0100

    RouteToolkit

---
+++ EveComFramework/KanedaToolkit/RouteToolkit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EveComFramework.Data;
+
+namespace EveComFramework.KanedaToolkit
+{
+    /// <summary>
+    /// Route Toolkit
+    /// </summary>
+    public class RouteToolkit
+    {
+        /// <summary>
+        /// Get minimum security status along a route
+        /// </summary>
+        /// <param name="routeList">List of solarSystemIDs along the route</param>
+        /// <returns>minimum security status</returns>
+        public static double RouteSecurity(List<long> routeList)
+        {
+            return SolarSystem.All.Where(a => routeList.Contains(a.ID)).Select(a => a.Security).Min();
+        }
+    }
+}
